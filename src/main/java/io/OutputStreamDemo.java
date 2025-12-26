35bcc089ef124566d226c699c80a78da8bce0d29
Tan Yawen <2749934612@qq.com>
2025-12-26 18:29:30 +0800

    输出流FileOutputStream(追加)

--- src/main/java/io/OutputStreamDemo.java
+++ src/main/java/io/OutputStreamDemo.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 
 public class OutputStreamDemo {
     public static void main(String[] args) {
-        outputTofile("hello world","E:/SuanFaDemo/SuanFaDemo/src/main/resources/output01.txt");
-
+//        outputTofile("hello world","E:/SuanFaDemo/SuanFaDemo/src/main/resources/output01.txt");
+        appendTofile(" Angela!","E:/SuanFaDemo/SuanFaDemo/src/main/resources/output01.txt");
     }
 
-    //写入内容到文件
+    //写入内容到文件(覆盖)
     public static void outputTofile(String content,String filePath) {
         FileOutputStream out=null;
         try{
@@ -19,7 +19,7 @@ public class OutputStreamDemo {
             if(!file.exists()){
                 file.createNewFile();
             }
-            out=new FileOutputStream(file);
+            out=new FileOutputStream(file);//即使你后边没有write，文件原本内容也会被清空
             String str=content;
             byte[] bytes=str.getBytes();
             out.write(bytes);
@@ -35,6 +35,19 @@ public class OutputStreamDemo {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    //追加内容到文件（推荐追加方式）
+    public static void appendTofile(String content,String filePath) {
+        try{
+            FileOutputStream out=new FileOutputStream(filePath,true);
+            out.write(content.getBytes());
+            out.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 
