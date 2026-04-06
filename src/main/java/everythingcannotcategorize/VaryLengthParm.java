891f7d78b72ab3ef449fd5e93721046d1ed8ec10
Tan Yawen <2749934612@qq.com>
2026-04-06 14:52:10 +0800

    可变参数

---
+++ src/main/java/everythingcannotcategorize/VaryLengthParm.java
@@ -0,0 +1,24 @@
+package everythingcannotcategorize;
+
+/*
+可变参数
+ */
+public class VaryLengthParm {
+    public static void main(String[] args) {
+        test(3,1,2,3);
+    }
+
+    /**
+     * 可变参数,本质上就是一个数组
+     * @param a
+     * @param b
+     */
+    public static void test(int a,int... b){
+        System.out.println(a);
+        for (int i : b) {
+            System.out.println(i);
+        }
+        System.out.println(b.length);
+
+    }
+}
